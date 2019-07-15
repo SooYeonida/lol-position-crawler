e7304e702958e7ee35d8ac014630cd5728bab233
sooyeon <tndus130@ajou.ac.kr>
2019-07-16 00:57:33 +0900

    add Summoner

--- src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/domain/Summoner.java
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/domain/Summoner.java
@@ -9,13 +9,13 @@ import java.util.List;
 @Data
 public class Summoner {
 
-      //private int profileIconId;
-      //private String name;
-      //private String puuid;
-      //private long summonerLevel;
-      //private long revisionDate;
+      private int profileIconId;
+      private String name;
+      private String puuid;
+      private long summonerLevel;
+      private long revisionDate;
       private  String id;
-      //private  String accountId;
+      private  String accountId;
 
 
 }
