499a2c534cafd5bcee3781b1c48b8f5d1ec4a320
sooyeon <tndus130@ajou.ac.kr>
2019-07-16 00:56:03 +0900

    delete one controller and update controller code

--- src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/controller/PositionController.java
+++ src/main/java/org/ajou/realcoding/positioncrawler/positioncrawler/controller/PositionController.java
@@ -2,6 +2,7 @@ package org.ajou.realcoding.positioncrawler.positioncrawler.controller;
 
 import org.ajou.realcoding.positioncrawler.positioncrawler.PositionService.Service;
 import org.ajou.realcoding.positioncrawler.positioncrawler.domain.Position;
+import org.ajou.realcoding.positioncrawler.positioncrawler.domain.Summoner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,14 @@ public class PositionController {
     @Autowired
     Service service;
 
+    @GetMapping("/lol/summoner/v4/summoners/by-name/{summonerName}")
+    public Summoner getSummonerInfo(@PathVariable String summonerName)
+    {
+        return service.summonerinfo(summonerName);
+    }
+
     @GetMapping("/position-info-crawler/positions/by-name/{summonerName}")
-    public List<Position> getPositioninfo(@PathVariable String summonerName)
+    public List<Position> getPositioninfo(@PathVariable String summonerName) //List<position>
     {
         return service.getPositionInfoByName(summonerName);
     }
